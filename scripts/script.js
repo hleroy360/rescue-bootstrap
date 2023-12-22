d3b84fd02453c7e89a10364615bb2bbabc976a33
hjleroy <hjleroy@unh.edu>
2023-12-21 22:45:34 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Helix LeRoy
+    File Name: script.js
+    Date: 12/21/2023
+*/
+
+//jQuery for hero image to consume the header window space
+$( document ).ready(function() {
+  $('.hero').height($(window).height());
+});
